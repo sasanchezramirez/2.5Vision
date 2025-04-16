48b9aee287bcee94b97b1c3be99dc9c3d83469a8
santiago <sasanchezramirez@gmail.com>
2025-04-15 20:18:19 -0500

    fix: cors

--- app/application/fast_api.py
+++ app/application/fast_api.py
@@ -22,7 +22,7 @@ def create_app() -> FastAPI:
     # Configuración de CORS
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["http://localhost", "http://localhost:3000", "http://localhost:8000", "http://127.0.0.1:3000", "http://127.0.0.1:8000", "http://localhost:4200"],
+        allow_origins=["http://localhost", "http://localhost:3000", "http://localhost:8000", "http://127.0.0.1:3000", "http://127.0.0.1:8000", "http://localhost:4200", "https://vision.gaugelife.co"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
