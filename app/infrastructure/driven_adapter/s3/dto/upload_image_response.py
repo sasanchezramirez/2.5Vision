7166e0e73f588e2f38dd2ac7d11d6b88bf1562c6
sasanchezramirez <sasanchezramirez@gmail.com>
2025-10-07 20:17:01 -0500

    refactor: take out hexagonal architechutre for this solution

--- app/infrastructure/driven_adapter/s3/dto/upload_image_response.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel
-
-class UploadImageResponse(BaseModel):
-    image_url: str
-    image_name: str
--- app/infrastructure/entry_point/dto/response_dto.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-from typing import Any, Optional
-
-class ResponseDTO(BaseModel):
-    apiCode: str
-    data: Optional[Any] = None
-    message: Optional[str] = None
-    status: Optional[bool] = None
